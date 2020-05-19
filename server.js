6106c41ab9aa1454a72b47b9b5ea177fb0ba60ff
JakeG-9191 <jacobgarlick@msn.com>
2020-05-19 15:31:05 -0600

    building skeleton out for application, included updated server, new component section, models for future database and config environments

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const users = require('./routes/users');
+// const posts = require('./routes/posts');
+// const config = require('config');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// app.use('/api/users', users);
+// app.use('/api/posts', posts);
+
+// const db = config.get('db');
+
+// mongoose.connect(db).then(() => console.log(`Connected to ${db}`));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}...`));
